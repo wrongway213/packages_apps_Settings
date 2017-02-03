fdb762e732bd4921b778ff42730f5eb5a662b240
ezio84 <brabus84@gmail.com>
2017-02-03 16:40:55 +0100

    Allow to quickly open Running Services from notification/qs panel [2/2]
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -169,4 +169,5 @@ public class Settings extends SettingsActivity {
     public static class SmartbarSettingsActivity extends SettingsActivity { /* empty */ }
     public static class PulseSettingsActivity extends SettingsActivity { /* empty */ }
     public static class PieControlSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
