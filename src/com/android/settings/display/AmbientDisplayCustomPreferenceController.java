DirtyUnicorns/android_packages_apps_Settings

Remove AOSP Ambient display conditionally
The OnePlus devices we currently support (OP3/T and OP5) both use a alterative
doze package. Having this under Settings/Display will not only confuse the user
and possibly cause problems.

- Add preference and controller for custom doze package (com.custom.ambient.display)

Change-Id: Ib3484de1e8c96bdad4c553af08b24cc14fbf5c43

src/com/android/settings/display/AmbientDisplayCustomPreferenceController.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.util.du.Utils;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class AmbientDisplayCustomPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private static final String KEY_AMBIENT_DISPLAY_CUSTOM = "ambient_display_custom";
+
+    public AmbientDisplayCustomPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_AMBIENT_DISPLAY_CUSTOM;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        return false;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Utils.hasAltAmbientDisplay(mContext.getApplicationContext());
+    }
+}

src/com/android/settings/display/AmbientDisplayPreferenceController.java
@@ -20,6 +20,7 @@ import android.os.UserHandle;
 import android.support.v7.preference.Preference;
 
 import com.android.internal.hardware.AmbientDisplayConfiguration;
+import com.android.internal.util.du.Utils;
 import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -41,7 +42,7 @@ public class AmbientDisplayPreferenceController extends AbstractPreferenceContro
 
     @Override
     public boolean isAvailable() {
-        return mConfig.available();
+        return mConfig.available() && !Utils.hasAltAmbientDisplay(mContext.getApplicationContext());
     }
 
     @Override

wrongway213/packages_apps_Settings

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

src/com/android/settings/display/AmbientDisplayCustomPreferenceController.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.util.screwd.screwdUtils;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class AmbientDisplayCustomPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private static final String KEY_AMBIENT_DISPLAY_CUSTOM = "ambient_display_custom";
+
+    public AmbientDisplayCustomPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_AMBIENT_DISPLAY_CUSTOM;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        return false;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return screwdUtils.hasAltAmbientDisplay(mContext.getApplicationContext());
+    }
+}

src/com/android/settings/display/AmbientDisplayPreferenceController.java
@@ -20,6 +20,7 @@ import android.os.UserHandle;
 import android.support.v7.preference.Preference;
 
 import com.android.internal.hardware.AmbientDisplayConfiguration;
+import com.android.internal.util.screwd.screwdUtils;
 import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -41,7 +42,7 @@ public class AmbientDisplayPreferenceController extends AbstractPreferenceContro
 
     @Override
     public boolean isAvailable() {
-        return mConfig.available();
+        return mConfig.available() && !screwdUtils.hasAltAmbientDisplay(mContext.getApplicationContext());
     }
 
     @Override