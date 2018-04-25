55440fda7a9d07c19c405cf56205736a0eea6251
Chris <chris@cypheros.co>
2018-04-25 15:51:45 -0400

    deviceinfo: Add support for overriding vendor properties
    Some properties will forcefully be read from vendor instead of system
    which can result in outdated or misrepresented OEM information. As a
    small workaround, allow overriding the current props that are displayed
    in about phone.
    
    This way we can set our own props and display proper information such as
    the security patch level. Although this is minor, asthetics matter.
    Current supported props are
    
    "ro.vendor.override.security_patch"
    "ro.vendor.override.build_display"
    
    To override the device model, simply add this string overlay with the
    desired model "config_overridenVendorProductModel"
    
    This is a last option incase we can't override these traditionally
    
    Change-Id: Ia00a23e45364e1546d5f5ebe55867b1c27340450
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -21,6 +21,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.support.v7.preference.Preference;
@@ -59,6 +60,7 @@ public class BuildNumberPreferenceController extends AbstractPreferenceControlle
     private final Fragment mFragment;
     private final UserManager mUm;
     private final MetricsFeatureProvider mMetricsFeatureProvider;
+    private final String mBuildNumberOverride;
 
     private Toast mDevHitToast;
     private RestrictedLockUtils.EnforcedAdmin mDebuggingFeaturesDisallowedAdmin;
@@ -91,6 +93,7 @@ public class BuildNumberPreferenceController extends AbstractPreferenceControlle
         mFragment = fragment;
         mUm = (UserManager) context.getSystemService(Context.USER_SERVICE);
         mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
+        mBuildNumberOverride = SystemProperties.get("ro.vendor.override.build_display", "");
         if (lifecycle != null) {
             lifecycle.addObserver(this);
         }
@@ -102,7 +105,11 @@ public class BuildNumberPreferenceController extends AbstractPreferenceControlle
         final Preference preference = screen.findPreference(KEY_BUILD_NUMBER);
         if (preference != null) {
             try {
-                preference.setSummary(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+                if (!TextUtils.isEmpty(mBuildNumberOverride)) {
+                    preference.setSummary(BidiFormatter.getInstance().unicodeWrap(mBuildNumberOverride));
+                } else {
+                    preference.setSummary(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+                }
                 preference.setEnabled(true);
             } catch (Exception e) {
                 preference.setSummary(R.string.device_info_default);
